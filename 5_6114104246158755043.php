e4e2be9bb7df16ec434d24c7d5ea5bc586ee8f33
Manjotm <99398976+Manjotm@users.noreply.github.com>
2022-03-01 09:48:40 +0530

    Add files via upload

---
+++ 5_6114104246158755043.php
@@ -0,0 +1,577 @@
+<!DOCTYPE html>
+<html>
+    </script> </center> </html> <div id="formFooter"> <a class="underlineHover" href="https://youtube.com/channel/UCPVtT0yDD-QaAEH6f0oW7Iw">Yt Channel
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title> Earning Area </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="http://scripter-master.cf/SM/IMG_20220227_200053_744.jpg" rel="icon" type="image/x-icon"/>
+  <meta name="description" content="Online Refer Bypass Script. Refer Bypass Script. Task Bypass Script.">
+  <meta name="author" content="Online Refer Script, Refer Bypass Script, Task Bypass Script.">
+  <meta name="keywords" content="Online Refer Script, Refer Bypass Script, Task Bypass Script.">
+</head>
+<style type="text/css">
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+@import url('https://fonts.googleapis.com/css2?family=Solway&display=swap');
+
+/* BASIC */
+
+*{ 
+ margin: 0;
+ padding: 0;
+ box-sizing: border-box;
+ font-family: "Poppins", sans-serif;
+}
+
+*:focus {
+    outline: none;
+} 
+
+:root { 
+ --main-color: #FF0001;
+ --shadow-color: rgba(93, 38, 193, 0.4);
+}
+
+body { 
+ width: 100%;
+ height: 100vh;
+ background-image: linear-gradient(45deg, var(--main-color) 45%, #ffffff 0%);
+ background-repeat: no-repeat; 
+}
+
+
+/* STRUCTURE  */
+
+.wrapper {
+ width: 100%;
+ min-height: 100%;
+ padding: 80px 20px 20px 20px;
+}
+
+/* ANIMATIONS */
+
+/* Simple CSS3 Fade-in-down Animation */
+.fadeInDown {
+  animation-name: fadeInDown;
+  animation-duration: 2s;
+  animation-fill-mode: both;
+}
+
+@keyframes fadeInDown {
+  0% {
+    opacity: 0;
+    transform: translate3d(0, -100%, 0);
+  }
+  100% {
+    opacity: 1;
+    transform: none;
+  }
+}
+
+
+#formContent {
+  border-radius: 10px;
+  background: #fff;
+  width: 95%;
+  max-width: 400px;
+  min-height: 350px;
+  max-height: 450px;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+}
+
+#navbarId { 
+  text-align: center;   
+}
+
+img {
+  width: 25%;
+  margin: 20px auto 5px auto;
+  padding-top: 30px;
+  display: block;
+}
+
+h2 {
+  text-align: center;
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: uppercase;
+  display: inline-block;
+  letter-spacing: 0.4px;
+  margin: 5px 8px 20px 8px; 
+  color: #636e72;
+  border-bottom: 1px solid #636e72;
+}
+
+
+input[type=text],select {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 12px 32px;
+  text-align: left;
+  text-decoration: none;
+  display: block;
+  font-size: 14px;
+  margin: 5px auto;
+  width: 85%;
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+}
+
+input[type=text]:focus {
+  background-color: #fff;
+  border-bottom: 2px solid var(--main-color);
+}
+
+input[type=text]:placeholder {
+  color: #cccccc;
+  }
+  
+  input[type=number],select {
+  background-color: #f6f6f6;
+  border: none;
+  color: #0d0d0d;
+  padding: 12px 32px;
+  text-align: left;
+  text-decoration: none;
+  display: block;
+  font-size: 14px;
+  margin: 5px auto;
+  width: 85%;
+  border: 2px solid #f6f6f6;
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+}
+
+input[type=number]:focus {
+  background-color: #fff;
+  border-bottom: 2px solid var(--main-color);
+}
+
+input[type=number]:placeholder {
+  color: #cccccc;
+}
+
+
+/* FORM TYPOGRAPHY*/
+
+input[type=button], input[type=submit], input[type=reset]  {
+  background-color: var(--main-color);
+  border: none;
+  color: Red;
+  letter-spacing: 1px;
+  padding: 12px 80px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+  text-transform: uppercase;
+  font-size: 13px;
+  -webkit-box-shadow: 0 10px 30px 0 var(--shadow-color);
+  box-shadow: 0 10px 30px 0 var(--shadow-color);
+  -webkit-border-radius: 5px 5px 5px 5px;
+  border-radius: 5px 5px 5px 5px;
+  margin: 5px auto 35px auto;
+  -webkit-transition: all 0.3s ease-in-out;
+  -moz-transition: all 0.3s ease-in-out;
+  -ms-transition: all 0.3s ease-in-out;
+  -o-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+}
+
+input[type=button]:hover, input[type=submit]:hover, input[type=reset]:hover  {
+  background-color: var(--main-color);
+}
+
+input[type=button]:active, input[type=submit]:active, input[type=reset]:active  {
+  -moz-transform: scale(0.95);
+  -webkit-transform: scale(0.95);
+  -o-transform: scale(0.95);
+  -ms-transform: scale(0.95);
+  transform: scale(0.95);
+}
+
+/* Simple CSS3 Fade-in Animation */
+@-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+@-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+@keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+
+.fadeIn {
+  opacity:0;
+  -webkit-animation:fadeIn ease-in 1;
+  -moz-animation:fadeIn ease-in 1;
+  animation:fadeIn ease-in 1;
+
+  -webkit-animation-fill-mode:forwards;
+  -moz-animation-fill-mode:forwards;
+  animation-fill-mode:forwards;
+
+  -webkit-animation-duration:1s;
+  -moz-animation-duration:1s;
+  animation-duration:1s;
+}
+
+.fadeIn.first {
+  -webkit-animation-delay: 0.4s;
+  -moz-animation-delay: 0.4s;
+  animation-delay: 0.4s;
+}
+
+.fadeIn.second {
+  -webkit-animation-delay: 0.6s;
+  -moz-animation-delay: 0.6s;
+  animation-delay: 0.6s;
+}
+
+.fadeIn.third {
+  -webkit-animation-delay: 0.8s;
+  -moz-animation-delay: 0.8s;
+  animation-delay: 0.8s;
+}
+
+.fadeIn.fourth {
+  -webkit-animation-delay: 1s;
+  -moz-animation-delay: 1s;
+  animation-delay: 1s;
+}
+
+
+#formFooter {
+  background-color: #f6f6f6;
+  border-top: 1px solid #dce8f1;
+  padding: 25px;
+  text-align: center;
+  border-radius: 0 0 10px 10px;
+}
+
+a {
+  color: var(--main-color);
+  display:inline-block;
+  text-decoration: none;
+  font-weight: 400;
+}
+
+/* Simple CSS3 Fade-in Animation */
+.underlineHover:after {
+  display: block;
+  left: 0;
+  bottom: -10px;
+  width: 0;
+  height: 2px;
+  background-color: var(--main-color);
+  content: "";
+  transition: width 0.2s;
+}
+
+.underlineHover:hover {
+  color: #0d0d0d;
+}
+
+.underlineHover:hover:after{
+  width: 100%;
+}
+
+.small_text_msg {
+  margin: 0px 0px 10px 25px;
+  color: #636e72;
+  font-size: 12px;
+  display: block;
+  text-align: left;
+}
+
+.BoxContent {
+  background-color: #f6f6f6;
+  width: 85%;
+  min-height: 30px;
+  padding: 15px;
+  margin: 5px auto 20px auto;
+  border-radius: 5px;
+}
+
+.BoxContent p {
+  padding: 10px 0px;
+  border-top: thin solid #ffffff;
+  text-align: center;
+  font-size: 12px;
+  color: #636e72;
+  font-family: 'Solway', serif;
+}
+
+pre {
+  font-size: 13px;
+}
+
+</style>
+
+</style>
+<body>
+ 
+        <div class="wrapper fadeInDown"> <div id="formContent"> <div class="fadeIn first" id="navbarId">
+       <img src="http://scripter-master.cf/SM/IMG_20220227_200053_744.jpg" alt="Icon">
+        
+        
+        <h2 class="active"><font color='blue'> T GamerX <font color='blue'> Refer Bypass<font color='blue'>  Script </h2><br><br>
+        
+        
+      </div>
+
+
+ 
+
+<center>
+   
+
+     <?php     
+
+function rando($length) {
+
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+    $charactersLength = strlen($characters);
+
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+
+    }
+
+    return $randomString;
+
+}
+
+function encrypt($data){
+
+  $key = rando(16);
+
+  return ''.$key.''.base64_encode(openssl_encrypt($data, "aes-128-ecb", $key, OPENSSL_RAW_DATA).'');
+
+}
+
+function decrypt($data){
+
+  $key = substr($data,0,16);
+
+  $data = substr($data, 16);
+
+  return openssl_decrypt(base64_decode($data), "aes-128-ecb", $key, OPENSSL_RAW_DATA);
+
+}
+
+?>
+
+<?php
+
+error_reporting(0);
+
+
+
+
+
+if(isset($_REQUEST['submit'])){
+
+	header('refresh: 1');
+
+ 
+
+  
+
+  $token=$_REQUEST["token"];
+  $pro=$_REQUEST["coin"];
+
+$ip=long2ip(rand());
+
+
+
+$n=15;
+
+function getName($n) {
+
+    $characters = '0123456789';
+
+    $randomString = '';
+
+  
+
+    for ($i = 0; $i < $n; $i++) {
+
+        $index = rand(0, strlen($characters) - 1);
+
+        $randomString .= $characters[$index];
+
+    }
+
+  
+
+    return $randomString;
+
+}
+
+  
+
+$a=getName($n);
+
+
+
+function generateRandomString($length = 15) {
+
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+    $charactersLength = strlen($characters);
+
+    $randomString = '';
+
+    for ($i = 0; $i < $length; $i++) {
+
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+
+    }
+
+    return $randomString;
+
+}
+
+//usage 
+
+$myRandomString = generateRandomString(15);
+$myRandomStrinng = generateRandomString(3);
+
+
+$d_info="udid=$8myRandomString||name=OPPjO||model=CPH1701||version=2.1";
+
+$a="amount=200||title=Reffer $myRandomStrinng:";
+
+$enca=encrypt($a);
+
+$urla=urlencode($enca);
+
+$encdinfo=encrypt($d_info);
+
+$urldinfo=urlencode($encdinfo);
+
+$dectoken=decrypt($token);
+
+
+
+$enctoken=encrypt($dectoken);
+
+
+
+$url="https://bestapp.ga/t_gamerx/api/v1/serv1/add_coin.php";
+
+$data="data=$urla&d_info=$urldinfo";
+
+$dd=strlen($data);
+
+$headers=["token: $enctoken","Content-Type: application/x-www-form-urlencoded","Content-Length: $dd","Host: bestapp.ga","Connection: Keep-Alive","Accept-Encoding: gzip","User-Agent: okhttp/3.14.9","X-Forwarded-For: $ip"];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL,$url);
+
+curl_setopt($ch, CURLOPT_POST, 1);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+
+curl_setopt($ch,CURLOPT_HEADER,0);
+
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+ curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+
+   curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+
+   curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+
+
+
+ $output= curl_exec ($ch);
+
+
+
+
+
+ curl_close ($ch);
+
+$aaa=decrypt($output);
+
+$json=json_decode($aaa,true);
+
+
+
+$msg=$json['status']['message'];
+
+
+
+echo " <center><font color='Blue'>$msg</center></font>";
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(!isset($_REQUEST['submit'])){
+
+
+
+    echo'	<center>
+
+                <form action="" method="POST">
+
+                        
+
+    <center><input placeholder="Enter Token" name="token" type="text" class="submit1" Required></center>
+
+   
+
+<center>
+
+        
+
+                    <cemter><input type="submit" class="submit" name="submit" value="Submit"></center>
+
+                </form>';
+
+    }
+?>
+	 </script> </center> </html> <div id="formFooter"> <a class="underlineHover" href="https://t.me/EarningAream">Join Our Telegram Channel </a> </div>
+	</script> </center> </html> <div id="formFooter"> <a class="underlineHover" href="https://t.me/Scripter_Master"></a> </diV
+	 
+	  </script> </center> </html> <div id="formFooter"> <a class="underlineHover" href="https://youtube.com/channel/UCPVtT0yDD-QaAEH6f0oW7Iw">Yt Channel
